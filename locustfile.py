8a1ab5a99ce4c9b803b29560a8bc1cd788d3f3ff
Jonathan Healy <jonathan.d.healy@gmail.com>
2022-03-17 05:50:44 -0700

    add tags

--- locustfile.py
+++ locustfile.py
@@ -1,10 +1,10 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task, constant, tag
 import random
 import json
 
 class WebsiteTestUser(HttpUser):
 
-    # wait_time = between(0.0001, 0.0002)
+    wait_time = constant(0.01)
     
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
@@ -19,22 +19,27 @@ class WebsiteTestUser(HttpUser):
         data = json.load(f)
         return data
 
+    @tag('root_catalog')
     @task(1)
     def get_root_catalog(self):
         self.client.get("http://localhost:8083")
 
+    @tag('all_collections')
     @task(2)
     def get_all_collections(self):
         self.client.get("http://localhost:8083/collections")
 
+    @tag('get_collection')
     @task(3)
     def get_collection(self):
         self.client.get("http://localhost:8083/collections/test-collection")
 
+    @tag('item_collection')
     @task(4)
     def get_item_collection(self):
         self.client.get("http://localhost:8083/collections/test-collection/items")
 
+    @tag('get_item')
     @task(5)
     def get_item(self):
         random_number = random.randint(1, 11)
@@ -42,14 +47,17 @@ class WebsiteTestUser(HttpUser):
         random_id = item["features"][random_number]["id"]
         self.client.get(f"http://localhost:8083/collections/test-collection/items/{random_id}", name="/item")
 
+    @tag('get_bbox')
     @task(6)
     def get_bbox_search(self):
         self.client.get("http://localhost:8083/search?bbox=-16.171875,-79.095963,179.992188,19.824820")
 
+    @tag('post_bbox')
     @task(7)
     def post_bbox_search(self):
         self.client.post("http://localhost:8083/search", json={"bbox":[16.171875,-79.095963,179.992188,19.824820]}, name="bbox")
 
+    @tag('point_intersects')
     @task(8)
     def post_intersects_search(self):
         self.client.post(
