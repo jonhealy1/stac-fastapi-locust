d49c1ad5f1756e9bc8c8685e4252640a9924f52b
jonhealy1 <jonathan.d.healy@gmail.com>
2022-03-17 07:48:45 -0700

    remove host path

--- locustfile.py
+++ locustfile.py
@@ -24,22 +24,22 @@ class WebsiteTestUser(HttpUser):
     @tag('root_catalog')
     @task(1)
     def get_root_catalog(self):
-        self.client.get("http://localhost:8083")
+        self.client.get("/")
 
     @tag('all_collections')
     @task(2)
     def get_all_collections(self):
-        self.client.get("http://localhost:8083/collections")
+        self.client.get("/collections")
 
     @tag('get_collection')
     @task(3)
     def get_collection(self):
-        self.client.get("http://localhost:8083/collections/test-collection")
+        self.client.get("/collections/test-collection")
 
     @tag('item_collection')
     @task(4)
     def get_item_collection(self):
-        self.client.get("http://localhost:8083/collections/test-collection/items")
+        self.client.get("/collections/test-collection/items")
 
     @tag('get_item')
     @task(5)
@@ -47,23 +47,23 @@ class WebsiteTestUser(HttpUser):
         random_number = random.randint(1, 11)
         item = self.load_file('data_loader/setup_data/sentinel-s2-l2a-cogs_0_100.json')
         random_id = item["features"][random_number]["id"]
-        self.client.get(f"http://localhost:8083/collections/test-collection/items/{random_id}", name="/item")
+        self.client.get(f"/collections/test-collection/items/{random_id}", name="/item")
 
     @tag('get_bbox')
     @task(6)
     def get_bbox_search(self):
-        self.client.get("http://localhost:8083/search?bbox=-16.171875,-79.095963,179.992188,19.824820")
+        self.client.get("/search?bbox=-16.171875,-79.095963,179.992188,19.824820")
 
     @tag('post_bbox')
     @task(7)
     def post_bbox_search(self):
-        self.client.post("http://localhost:8083/search", json={"bbox":[16.171875,-79.095963,179.992188,19.824820]}, name="bbox")
+        self.client.post("/search", json={"bbox":[16.171875,-79.095963,179.992188,19.824820]}, name="bbox")
 
     @tag('point_intersects')
     @task(8)
     def post_intersects_search(self):
         self.client.post(
-            "http://localhost:8083/search", 
+            "/search", 
             json={
                 "collections":["test-collection"],
                 "intersects":{"type": "Point", "coordinates": [150.04, -33.14]}
@@ -80,7 +80,7 @@ class WebsiteTestUser(HttpUser):
         item["id"] = f"test-item-{random_number}"
         item["collection"] = "test-collection"
         self.client.post(
-            "http://localhost:8083/collections/test-collection/items",
+            "/collections/test-collection/items",
             json=item,
             name="create-item"
         )
